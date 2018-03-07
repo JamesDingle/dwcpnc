980a8e2256b474b06e9710ff65a5b671facfd479
James Dingle <jad@pml.ac.uk>
2018-03-07 16:58:48 +0000

    jad: added seasonal correction for spectral irradiance

--- solar.c
+++ solar.c
@@ -8,7 +8,7 @@
 
 
 double compute_sunrise(int jday, double lat) {
-    double day = (double)jday; // convert to double for the maths
+    double day = (double) jday; // convert to double for the maths
     double theta, delta, phi, phidel;
     theta = (2.0 * M_PI * day) / 365.0;
     delta = 0.006918 - 0.399912 * cos(theta) +
@@ -32,7 +32,7 @@ double compute_sunrise(int jday, double lat) {
 
 }
 
-double* zenith_array_from_times(time_profile_t *tprof, int julday, double lat) {
+double *zenith_array_from_times(time_profile_t *tprof, int julday, double lat) {
 
     double theta, delta, phi;
 
@@ -43,7 +43,7 @@ double* zenith_array_from_times(time_profile_t *tprof, int julday, double lat) {
 
     phi = lat * (M_PI / 180.);
 
-    double* zenith_r_array = (double*)malloc(sizeof(double) * tprof->count);
+    double *zenith_r_array = (double *) malloc(sizeof(double) * tprof->count);
 
     int i;
     for (i = 0; i < tprof->count; ++i) {
@@ -53,14 +53,14 @@ double* zenith_array_from_times(time_profile_t *tprof, int julday, double lat) {
     return zenith_r_array;
 }
 
-double compute_zenith(double local_time, double delta, double phi){
+double compute_zenith(double local_time, double delta, double phi) {
     double th;
     th = (local_time - 12) * (M_PI / 12);
 
     double zen;
     zen = sin(delta) * sin(phi) + cos(delta) * cos(phi) * cos(th);
 
-    if (zen < -1.0){
+    if (zen < -1.0) {
         zen = -1.0;
     } else if (zen > 1.0) {
         zen = 1.0;
@@ -69,4 +69,35 @@ double compute_zenith(double local_time, double delta, double phi){
     zen = (M_PI / 2.0) - asin(zen);
 
     return zen;
+}
+
+double thekaekara_correction(int jul_day) {
+
+    double *day_points = (double[25]) {
+            0., 3., 31., 42., 59., 78., 90., 93., 120., 133., 151., 170.,
+            181., 183., 206., 212., 243., 265., 273., 277., 304., 306.,
+            334., 355., 365.
+    };
+    double *ir_points = (double[25]) {
+            1399., 1399., 1393., 1389., 1378., 1364., 1355., 1353.,
+            1332., 1324., 1316., 1310., 1309., 1309., 1312., 1313.,
+            1329., 1344., 1350., 1353., 1347., 1375., 1392., 1398., 1399.
+    };
+
+    int i, idx = 0;
+    for (i = 0; i < 25; ++i) {
+        idx = i;
+        if (i >= jul_day) {
+            break;
+        }
+    }
+
+    double temp;
+
+    if (idx == 0) {
+        return ir_points[idx];
+    } else {
+        temp = (jul_day - day_points[idx -1]) / (day_points[idx] - day_points[idx - 1]);
+        return ir_points[idx -1] - (ir_points[idx-1] - ir_points[idx]) * temp;
+    }
 }
